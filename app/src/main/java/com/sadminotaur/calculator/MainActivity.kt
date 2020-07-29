1cf81fc80229766f82c695948c81dbad3dc23c5b
Dima <dimasilolil@gmail.com>
2020-07-29 17:34:25 +0600

    changed theme

--- app/src/main/java/com/sadminotaur/calculator/MainActivity.kt
+++ app/src/main/java/com/sadminotaur/calculator/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
 
     private lateinit var textView: TextView
+    private val inMemory: HashMap<Char, Double> = HashMap()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +24,24 @@ class MainActivity : AppCompatActivity() {
         val startText = if (textView.text.toString() == "0") "" else textView.text.toString()
         textView.text = startText + buttonText
     }
+
+    @SuppressLint("SetTextI18n")
+    fun buttonDot(view: View) {
+        val text = textView.text.toString()
+        if (text != "" && !text.contains(".")) {
+            textView.text = textView.text.toString() + "."
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun clean(view: View) {
+        textView.text = ""
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun actions(view: View) {
+        val buttonText = (view as Button).text.toString()
+        inMemory[buttonText[0]] = textView.text.toString().toDouble()
+        textView.text = ""
+    }
 }
-
-
