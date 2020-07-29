1d674bc931c43fe5b452eae0438ec13bf5033e70
Dima <dimasilolil@gmail.com>
2020-07-29 14:58:20 +0600

    working digits

--- app/src/main/java/com/sadminotaur/calculator/MainActivity.kt
+++ app/src/main/java/com/sadminotaur/calculator/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.sadminotaur.calculator
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.text)
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun buttonDigits(view: View) {
+        val buttonText = (view as Button).text
+        val startText = if (textView.text.toString() == "0") "" else textView.text.toString()
+        textView.text = startText + buttonText
     }
 }
+
+
